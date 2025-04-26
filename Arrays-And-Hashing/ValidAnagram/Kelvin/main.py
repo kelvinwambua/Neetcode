d053ca5fa3982944805a54380de3542542d5e326
kelvinwambua <shwiftykelvin@gmail.com>
2025-04-26 16:18:01 +0300

    Day 2

--- Arrays-And-Hashing/ValidAnagram/Kelvin/main.py
+++ Arrays-And-Hashing/ValidAnagram/Kelvin/main.py
@@ -1,12 +1,23 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        newString = []  
-        for i in range((len(s)-1), -1, -1):   
-            newString.append(s[i])  
-        if newString == t:
-            return True
-        else:
+        if len(s) != len(t):
             return False
-    solution = Solution()
-    print(solution.isAnagram("anagram", "nagaram"))
+        countS, countT = {}, {}
+        for i in range(len(s)):
+            countS[s[i]] = countS.get(s[i], 0) + 1
+            countT[t[i]] = countT.get(t[i], 0) + 1
+        for c in countS:
+            if countS[c] != countT.get([c],0):
+                return False
+   
 
+#Same as above but using Counter from collections
+class Solution(object):
+    def isAnagram(self, s, t):
+        return Counter(s) == Counter(t)
+   
+
+# Increases Time COmplexity but decreases Space Complexity as we do not declare a new memory structure
+class Solution(object):
+    def isAnagram(self, s, t):
+        return sorted(s) == sorted(t)
